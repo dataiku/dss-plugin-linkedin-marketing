4b3f1e35e95aed60a3df054ab72acc07a04377c9
admin <admin>
2020-05-20 13:52:35 +0200

    Edited file 'python-lib/api_call.py'

--- python-lib/api_call.py
+++ python-lib/api_call.py
@@ -1,15 +1,10 @@
 import requests
-import pandas as pd
 import numpy as np
 from math import ceil
 import logging
 
-# ==============================================================================
-# CLASS AND FUNCTION DEFINITION
-# ==============================================================================
 
-
-def get_query(headers: dict, ids: list() = [], granularity: str = "GROUP", batch_size: int = 1000) -> pd.DataFrame:
+def get_query(headers: dict, granularity: str = "GROUP",ids: list() = [], batch_size: int = 1000) -> list():
     """
     Perfom a Get query and return the data related to the creative or campaign ids given. When the query is too voluminous, lower the batch size to perform a batch query. 
 
@@ -20,10 +15,10 @@ def get_query(headers: dict, ids: list() = [], granularity: str = "GROUP", batch
         batch_size       Number of ids by batch query (ex - 100)
 
     Outputs: 
-        df               A Pandas dataframe containing the LinkedIn data related to the input ids
+        query_output     Output of the API call with the appropriate contents, for ex- dateRange, impressions... 
     """
 
-    initial_param = {"GROUP": {"q": "search","search.account.values[0]":"urn:li:sponsoredAccount:507690462"}, "CAMPAIGN": {"q": "search"}, "CREATIVES": {"q": "search"}, "CAMPAIGN_ANALYTICS": {"q": "analytics", "pivot": "CAMPAIGN", "dateRange.start.day": "1",
+    initial_param = {"GROUP": {"q": "search"}, "CAMPAIGN": {"q": "search"}, "CREATIVES": {"q": "search"}, "CAMPAIGN_ANALYTICS": {"q": "analytics", "pivot": "CAMPAIGN", "dateRange.start.day": "1",
                                                                                                                                  "dateRange.start.month": "1", "dateRange.start.year": "2006", "timeGranularity": "DAILY"}, "CREATIVES_ANALYTICS": {"q": "analytics", "pivot": "CREATIVE", "dateRange.start.day": "1", "dateRange.start.month": "1", "dateRange.start.year": "2006", "timeGranularity": "DAILY"}}
 
     try:
@@ -34,39 +29,16 @@ def get_query(headers: dict, ids: list() = [], granularity: str = "GROUP", batch
 
     count = len(ids)
     if count >= batch_size:
-        df = batch_query(batch_size, count, ids, headers,
+        query_output = batch_query(batch_size, ids, headers,
                          granularity, initial_param[granularity])
     else:
         params = {**initial_param[granularity], **
                   get_analytics_parameters(ids, granularity)}
-        df = query_to_df(headers, params, granularity)
-    return df
+        query_output = query(headers, params, granularity)
+    return query_output
 
 
-def batch_query(batch_size: int, count: int, ids: list(), headers: dict, granularity: str, initial_params: dict) -> pd.DataFrame:
-    """
-    Perfom a batch get query 
-
-    Inputs:
-        batch_size       Number of ids by batch query (ex - 100)
-        count            Number of ids 
-        headers          Headers of the GET query, containing the access token for the OAuth2 identification
-        granularity      Granularity of the data : GROUP, CAMPAIGN, CREATIVES, CAMPAIGN_ANALYTICS, CREATIVES_ANALYTICS
-        initial_params   Fefault parameters for the query. For ex -  q: search
-
-    Outputs: 
-        df               A Pandas dataframe containing the LinkedIn data related to the input ids
-    """
-    df = pd.DataFrame()
-    chunks = [*np.array_split(ids, ceil(count/batch_size))]
-    for batch in chunks:
-        params = {**initial_params, **
-                  get_analytics_parameters(batch, granularity)}
-        df = df.append(query_to_df(headers, params, granularity), sort=False)
-    return df
-
-
-def query_to_df(headers: dict, parameters: dict, granularity: str) -> pd.DataFrame:
+def query(headers: dict, parameters: dict, granularity: str) -> list():
     """
     Retrieve a dataframe with data pulled from the API
 
@@ -76,19 +48,14 @@ def query_to_df(headers: dict, parameters: dict, granularity: str) -> pd.DataFra
         granularity      Granularity of the data : GROUP, CAMPAIGN, CREATIVES, CAMPAIGN_ANALYTICS, CREATIVES_ANALYTICS
 
     Outputs: 
-        df               A Pandas dataframe containing the data from the LinkedIn Api 
+        query_output               A Pandas dataframe containing the data from the LinkedIn Api 
     """
     url = {"GROUP": "https://api.linkedin.com/v2/adCampaignGroupsV2", "CAMPAIGN": "https://api.linkedin.com/v2/adCampaignsV2/", "CAMPAIGN_ANALYTICS": "https://api.linkedin.com/v2/adAnalyticsV2",
            "CREATIVES": "https://api.linkedin.com/v2/adCreativesV2/", "CREATIVES_ANALYTICS": "https://api.linkedin.com/v2/adAnalyticsV2"}
 
     query = requests.get(url=url[granularity],
                          headers=headers, params=parameters)
-    try:
-        df = pd.DataFrame.from_dict(query.json()["elements"])
-        return df
-    except KeyError as e:
-        logging.error(e)
-        raise ValueError("Could not convert to dataframe: "+str(query.json()))
+    return query.json()
 
 
 def get_analytics_parameters(ids: list(), granularity: str) -> dict:
@@ -109,6 +76,34 @@ def get_analytics_parameters(ids: list(), granularity: str) -> dict:
     urn_prefix = {"GROUP": "", "CAMPAIGN": "urn:li:sponsoredCampaignGroup:", "CAMPAIGN_ANALYTICS": "urn:li:sponsoredCampaign:",
                   "CREATIVES": "urn:li:sponsoredCampaign:", "CREATIVES_ANALYTICS": "urn:li:sponsoredCreative:"}
     for i, id_value in enumerate(ids):
-        params[key_prefix[granularity].format(
-            str(i))] = urn_prefix[granularity] + str(id_value)
+        params[key_prefix[granularity].format(str(i))] = urn_prefix[granularity] + str(id_value)
     return params
+
+
+def batch_query(batch_size: int, ids: list(), headers: dict, granularity: str, initial_params: dict) -> list():
+    """
+    Perfom a batch get query 
+
+    Inputs:
+        batch_size       Number of ids by batch query (ex - 100)
+        ids              Ids of the queried entities  (campaign ids, group ids...)
+        headers          Headers of the GET query, containing the access token for the OAuth2 identification
+        granularity      Granularity of the data : GROUP, CAMPAIGN, CREATIVES, CAMPAIGN_ANALYTICS, CREATIVES_ANALYTICS
+        initial_params   Fefault parameters for the query. For ex -  q: search
+
+    Outputs: 
+        query_output     Output of the API call with the appropriate contents, for ex- dateRange, impressions... 
+    """
+    count = len(ids)
+    chunks = [*np.array_split(ids, ceil(count/batch_size))]
+    query_output = dict({'paging': {'start': 0, 'count': 0, 'links': [], 'total': 0},"elements":[],"exceptions":[]})
+    for batch in chunks:
+        params = {**initial_params, **
+                  get_analytics_parameters(batch, granularity)}
+        try:
+            query_output["elements"].extend(query(headers,params,granularity)["elements"])
+        except KeyError as e:
+            logging.info(e)
+            query_output["exceptions"].extend([{"id":id,"API_response":query(headers,params,granularity)} for id in batch])
+    return query_output
+
