0b7bced5396245b8e3ff8581643f5a2c1d9d65ff
admin <admin>
2020-05-20 13:53:57 +0200

    Edited file 'python-lib/plugin_io_utils.py'

---
+++ python-lib/plugin_io_utils.py
@@ -0,0 +1,7 @@
+def build_column_names(query:dict)->list():
+    try:
+        content_columns = query["elements"]
+        column_names = list(content_columns[0].keys()).append("API_response")
+    except KeyError:
+        column_names = ["API_response"]
+    return column_names
