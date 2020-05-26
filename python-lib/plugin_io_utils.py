491b356855c8509551f6c15f92ada33ae6c58d01
admin <admin>
2020-05-26 11:17:25 +0200

    Edited files

--- python-lib/plugin_io_utils.py
+++ python-lib/plugin_io_utils.py
@@ -1,7 +1,3 @@
-from api_call import get_query
-from api_format import LinkedInAPIFormatter
-
-
 def build_column_names(query:dict)->list():
     try:
         content_columns = query["elements"]
@@ -9,11 +5,3 @@ def build_column_names(query:dict)->list():
     except KeyError:
         column_names = ["API_response"]
     return column_names
-
-
-def check_account_id(account_id:int):
-    account = get_query(HEADERS, granularity = "ACCOUNT")
-    api_formatter = LinkedInAPIFormatter(group)
-    account_df = api_formatter.format_to_df()
-    if account_id not in account_df.id.values:
-        raise Exception("Wrong account id or not accessible with the current access token")
